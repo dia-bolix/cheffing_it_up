f43184a578c072b233ad0d648c4a4683090b0505
Alston Shi <alstonshi@Alstons-MacBook-Air.local>
2022-10-02 22:04:24 -0400

    Added setup we need to add a database in order to add more into food_info

---
+++ db/food_info.py
@@ -0,0 +1,19 @@
+"""
+This module encapsulates details about the food
+"""
+
+
+FOOD_INFO = {FOOD: {'Calories': 100, 'Vitamins': ABCD, 'ingredients' = Water}}
+
+def get_food_info(():
+    return list(FOOD_INTO.keys())
+
+def get_food_info_details(food):
+    return FOOD_INTO[food]
+
+def main():
+    char_types = get_food_info()
+    print(food)
+
+if __name__ == '__main__':
+    main()
---
+++ db/food_types.py
@@ -0,0 +1,10 @@
+
+"""
+This is a module to detail our food types 
+"""
+
+BREAKFAST = 'breakfast'
+LUNCH = 'lunch'
+DINNER = 'dinner'
+
+FOOD_TYPES = {BREAKFAST: {'
