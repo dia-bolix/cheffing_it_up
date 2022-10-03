332b5c852005d338ad6918f497d21f14e4da05f5
Alston Shi <ashi3454@bths.edu>
2022-10-02 23:16:07 -0400

    Add 3 food item, with calo, vitamin and ingred. still need step by step

--- db/food_info.py
+++ db/food_info.py
@@ -2,14 +2,11 @@
 This module encapsulates details about the food
 """
 
-
-
 listOfFoods = ["Apple Pie", "Pumpkin Pie", "Blueberry Muffin"]
 FOOD_INFO = {"APPLE PIE" : {'Calories': 67,'Vitamins': AC, 'Ingredients':Apples-wheat flour-oil-cream-sugar},
 	    "PUMPKIN PIE" : {'Calories': 323, 'Vitamins': B6, 'Ingredients': Pumpkin-flour-butter-egg-cornstarch=cinnamon-nutmeg-salt-milk-cream},
  	    "Blueberry Muffin": {'Calories': 467, 'Vitamins': AC, 'Ingredients': flour-blueberry-salt-baking power-oil-egg-vanilla extract-sugar-milk},}	
 FOOD_INFO_TEST = {FOOD: {'Calories': 100, 'Vitamins': ABCD, 'ingredients' = Water}}
-
 
 
 def get_food_info(():
