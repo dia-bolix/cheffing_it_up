838aeec52853e5dcf9c1b1a3030b10943fb51789
Alston Shi <ashi3454@bths.edu>
2022-10-09 19:43:56 -0400

    Spacing changes in foodmenu

--- db/foodmenu.py
+++ db/foodmenu.py
@@ -16,12 +16,16 @@ MICRONUTRIENTS = 'Micronutrients'
 
 REQUIRED_FLDS = [NAME, MEAL_OF_DAY, INGREDIENTS,
                  CALORIES, MACRONUTRIENTS, MICRONUTRIENTS]
-food = {TEST_MENU: {NAME: 'CAKE', MEAL_OF_DAY: 'ANY TIME', INGREDIENTS: "MAGIC, SUGAR", CALORIES: 10, MACRONUTRIENTS: "Fats: 1g, Carbs: 1g, Protein: 10g", 
+food = {TEST_MENU: {NAME: 'CAKE', MEAL_OF_DAY: 'ANY TIME', INGREDIENTS: "MAGIC, SUGAR", CALORIES: 10, 
+                        MACRONUTRIENTS: "Fats: 1g, Carbs: 1g, Protein: 10g", 
                         MICRONUTRIENTS: "Vitamin A: 100%, Vitamin C: 100%, Iron: 100%"},
              'Apple Pie': {NAME: "APPLE PIE", MEAL_OF_DAY: "Dessert", INGREDIENTS: "Apples, Wheat flour, oil, cream,sugar", CALORIES: 237,
-                           MACRONUTRIENTS: "Carbs: 43g, Protein: 2.4g, Fats: 14g", MICRONUTRIENTS: "Vitamin A: 3.1%, Vitamin C: 6.7%, Calcium: 1.1%, Iron: 3.1%"},
-             'Pumpkin Pie': {NAME: "PUMPKIN PIE", MEAL_OF_DAY: "DEssert", INGREDIENTS: "Pumpkin flour, butter, egg, cornstarch, cinnamon, nutmeg, salt, milk, cream",
-                             CALORIES: 323, MACRONUTRIENTS: "Fats: 13g, Carbs: 46g, Protein: 52.g", MICRONUTRIENTS: "Vitamin A: 91%, Calcium: 6.5%, Iron: 6.7%"},
+                           MACRONUTRIENTS: "Carbs: 43g, Protein: 2.4g, Fats: 14g", 
+                           MICRONUTRIENTS: "Vitamin A: 3.1%, Vitamin C: 6.7%, Calcium: 1.1%, Iron: 3.1%"},
+             'Pumpkin Pie': {NAME: "PUMPKIN PIE", MEAL_OF_DAY: "DEssert", 
+                            INGREDIENTS: "Pumpkin flour, butter, egg, cornstarch, cinnamon, nutmeg, salt, milk, cream",
+                             CALORIES: 323, MACRONUTRIENTS: "Fats: 13g, Carbs: 46g, Protein: 52.g", 
+                             MICRONUTRIENTS: "Vitamin A: 91%, Calcium: 6.5%, Iron: 6.7%"},
 
              }
 
@@ -29,11 +33,14 @@ food = {TEST_MENU: {NAME: 'CAKE', MEAL_OF_DAY: 'ANY TIME', INGREDIENTS: "MAGIC, 
 def menu_exists(name):
     return name in food
 
+
 def get_food():
     return list(food.keys())
 
+
 def get_food_details(name):
     return food.get(name, None)
+
 
 def add_food(name, details):
     if not isinstance(name, str):
@@ -45,10 +52,12 @@ def add_food(name, details):
             raise ValueError(f'Requried {field=} missing from details')
     food[name] = details
 
+
 def main():
     food = get_food()
     print(f'{food=}')
     print(f'{get_food_details(TEST_MENU)=}')
+
 
 if __name__ == '__main__':
     main()
