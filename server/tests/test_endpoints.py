a1cc632d99369d14eaae1ae90c0ab2b95490eaa1
Alston Shi <ashi3454@bths.edu>
2022-10-09 21:38:54 -0400

    adding to test_endpoints.py

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -3,7 +3,7 @@ import pytest
 import server.endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
-
+TEST_FOOD_TYPE = 'Breakfast'
 
 def test_hello():
     """
@@ -13,7 +13,7 @@ def test_hello():
     assert isinstance(resp_json[ep.MESSAGE], str)
 
 
-def test_get_character_type_list():
+def test_get_food_type_list():
     """
     See if we can get a charcter type list properly.
     Return should look like:
@@ -23,7 +23,7 @@ def test_get_character_type_list():
     assert isinstance(resp_json[ep.FOOD_TYPE_LIST_NM], list)
 
 
-def test_get_character_type_list_not_empty():
+def test_get_food_type_list_not_empty():
     """
     See if we can get a charcter type list properly.
     Return should look like:
@@ -31,4 +31,13 @@ def test_get_character_type_list_not_empty():
     """
     resp_json = TEST_CLIENT.get(ep.FOOD_TYPE_LIST).get_json()
     assert len(resp_json[ep.FOOD_TYPE_LIST_NM]) > 0
+
+
+def test_getfood_type_details():
+    """
+    testing testing
+    """
+    resp_json = TEST_CLIENT.get(f'{ep.FOOD_TYPE_DETAILS}/{TEST_FOOD_TYPE}').get_json()
+    assert TEST_FOOD_TYPE in resp_json
+    assert isinstance(resp_json[TEST_FOOD_TYPE], dict)
 
