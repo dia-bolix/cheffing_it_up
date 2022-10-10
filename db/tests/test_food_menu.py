1c3b3d54be3a83b217db8c45550bf6bfe01f2358
Alston Shi <ashi3454@bths.edu>
2022-10-09 23:29:41 -0400

    Add tests in db

---
+++ db/tests/test_food_menu.py
@@ -0,0 +1,39 @@
+from lib2to3.pytree import type_repr
+from msilib.schema import Error
+import pytest 
+
+import db.food_menu as fm
+
+
+def test_get_menu():
+    fms = fm.get_food()
+    assert isinstance(fms, list)
+    assert len(fms) > 1
+
+
+def test_get_menu_details():
+    fm_details = fm.get_food_details(fm.TEST_MENU)
+    assert isinstance(fm_details, dict)
+
+
+def test_add_wrong_name_type():
+    with pytest.raises(TypeError):
+        fm.add_food(7, {})
+
+
+def test_add_wrong_details_type():
+    with pytest.raises(TypeError):
+        fm.add_food('a new menu item', [])
+
+
+def test_add_missing_field():
+    with pytest.raises(ValueError):
+        fm.add_food('a new menu item', {'foo':'bar'})
+
+
+def test_add_menu():
+    details = {}
+    for field in fm.REQUIRED_FLDS:
+        details[field] = 2
+    fm.add_food(fm.TEST_MENU, details)
+    assert fm.menu_exists(fm.TEST_MENU)
---
+++ db/tests/test_food_types.py
@@ -0,0 +1,11 @@
+import db.food_types as ft
+
+def test_get_food_types():
+	food_type_list = ft.get_food_types()
+	assert isinstance(food_type_list, list)
+	assert len(food_type_list) > 1
+
+def test_get_food_info_details():
+	food_details_dict = ft.get_food_type_details("APPLE PIE")
+	assert isinstance(food_details_dict, dict)
+	assert len(food_details_dict) > 1
