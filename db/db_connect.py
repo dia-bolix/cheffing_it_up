584561dc00d75891bfa1c3ec3046c1a89ac51eeb
Alston Shi <ashi3454@bths.edu>
2022-12-11 12:19:19 -0500

    Mongo connect

--- db/db_connect.py
+++ db/db_connect.py
@@ -22,6 +22,15 @@ def connect_db():
     if client is None:  # not connected yet!
         print("Setting client because it is None.")
         if os.environ.get("LOCAL_MONGO", LOCAL) == LOCAL:
+            password = os.environ.get("GAME_MONGO_PW")
+            if not password:
+                raise ValueError('You must set your password '
+                                 + 'to use Mongo in the cloud.')
+            print("Connecting to Mongo in the cloud.")
+            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
+                                    + '@cluster0.eqxbbqd.mongodb.net/'
+                                    + '?retryWrites=true&w=majority')
+        else:
             print("Connecting to Mongo locally.")
             client = pm.MongoClient()
 
