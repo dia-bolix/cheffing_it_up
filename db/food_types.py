07f754f98149a53fa1193ded81d3459df020df8e
Alston Shi <ashi3454@bths.edu>
2023-05-03 12:52:36 -0400

    added a anytime time selection, checking if changes appear on front end

--- db/food_types.py
+++ db/food_types.py
@@ -6,10 +6,12 @@ This is a module to detail our food types
 BREAKFAST = 'breakfast'
 LUNCH = 'lunch'
 DINNER = 'dinner'
+ANYTIME = 'anytime'
 
 FOOD_TYPES = {BREAKFAST: {'time': "6am - 8am"},
               LUNCH: {'time': "11am-1pm"},
-              DINNER: {'time': "5pm-7pm"}, }
+              DINNER: {'time': "5pm-7pm"}, 
+              ANYTIME: {'time': "Any Time"}}
 
 
 def get_food_types():
