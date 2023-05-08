3e5bfc1dbb2f620d6e31edf05c7a881d1a062781
Alston Shi <ashi3454@bths.edu>
2023-05-08 00:19:15 -0400

    revert changes

--- db/users.py
+++ db/users.py
@@ -9,6 +9,7 @@ REQUIRED_FLDS = [EMAIL]
 
 
 def user_exists(name):
+    dbc.connect_db()
     """
     Returns whether or not a user exists.
     """
@@ -17,6 +18,7 @@ def user_exists(name):
 
 
 def add_user(name, password, details):
+    dbc.connect_db()
     missing_fields = [field for field in REQUIRED_FLDS if field not in details]
     if missing_fields:
         raise ValueError(f'Required fields {missing_fields} \
@@ -26,28 +28,34 @@ def add_user(name, password, details):
 
 
 def authenticate_user(name, password):
+    dbc.connect_db()
     return dbc.authenticate_user(name, password)
 
 
 def get_users():
+    dbc.connect_db()
     users = dbc.fetch_all(dbc.LOGIN_PW_COLLECTION)
     return [user[NAME] for user in users]
 
 
 def get_user_details(user):
+    dbc.connect_db()
     return dbc.fetch_one(dbc.LOGIN_PW_COLLECTION, {NAME: user})
 
 
 def del_user(name):
+    dbc.connect_db()
     dbc.del_one(dbc.LOGIN_PW_COLLECTION, {NAME: name})
 
 
 def delete_user(username):
+    dbc.connect_db()
     """Deletes a user from the database."""
     dbc.del_one(dbc.LOGIN_PW_COLLECTION, {NAME: username})
 
 
 def main():
+    dbc.connect_db()
     users = get_users()
     print(f'{users=}')
     print(f'{get_user_details("Test user")=}')
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -91,21 +91,20 @@ def test_get_food_type_list_not_empty():
 #     else:
 #         assert False, 'Response is None'
 
+# def test_add_user():
+#     """
+#     Test adding a user.
+#     """
+#     resp = TEST_CLIENT.post('/users/register', json=SAMPLE_USER)
 
-def test_add_user():
-    dbc.connect_db()
-    response = TEST_CLIENT.post('/users/register', json={
-        'username': SAMPLE_USER_NM,
-        'password': SAMPLE_USER_PW,
-        'email': SAMPLE_USER_EMAIL,
-        'full_name': SAMPLE_USER_FULL_NAME
-    })
+#     # Check if the user has been added successfully
+#     assert resp.status_code == HTTPStatus.CREATED
 
-    # Check if the user has been added successfully
-    assert response.status_code == HTTPStatus.CREATED
+#     # Check if the user exists
+#     assert usr.user_exists(SAMPLE_USER_NM)
 
-    # Check if the user exists
-    assert usr.user_exists(SAMPLE_USER_NM)
+#     # Clean up by deleting the user
+#     usr.del_user(SAMPLE_USER_NM)
 
 
 def test_get_user_list():
